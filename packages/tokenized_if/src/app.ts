63b3164ca2f1a7e5931958876d1770a0b6cb4f96
Bart van Schaick <B.T.J.vanSchaick@student.tudelft.nl>
2021-03-31 20:31:52 +0200

    Add pino logging

--- packages/tokenized_if/src/app.ts
+++ packages/tokenized_if/src/app.ts
@@ -1,14 +1,25 @@
 import express from "express";
+
 import apiRouter from "./routes";
+import { getLogger } from "./logging";
 
 const port = parseInt(process.env.PORT) || 3000;
 
 const app = express();
 
+// Use middleware for parsing message bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("App works"));
+// Add http message logging
+const logger = getLogger("http req/res");
+app.use(logger);
+
+// Add routing
+app.get("/", (req, res) => {
+  res.send("App works");
+});
 app.use("/api", apiRouter);
 
+// Start listening
 app.listen(port, () => console.log(`App listening on port ${port}`));
