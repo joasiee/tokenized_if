74cc4a23811bfc2715ba0c13f3bf2c3d5a2bf438
Bjorn Ho <B.I.Y.L.Ho@student.tudelft.nl>
2021-04-06 02:05:55 +0200

    token manager export

---
+++ packages/tokenized_if/src/services/token.ts
@@ -0,0 +1,5 @@
+import {TokenManager} from "../../../token_registry/"
+import dotenv from "dotenv";
+dotenv.config();
+
+export const TM = new TokenManager(process.env.GANACHE_URL, process.env.PRIVATE_KEY);
