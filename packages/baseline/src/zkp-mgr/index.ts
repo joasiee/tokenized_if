12e1b445d92b0507e23f7855e81a0cc80e908cc7
Joas Mulder <joasmulder@hotmail.com>
2021-04-08 15:31:27 +0200

    hashing util -> shared

--- packages/baseline/src/zkp-mgr/index.ts
+++ packages/baseline/src/zkp-mgr/index.ts
@@ -1,16 +1,13 @@
 import { schema } from "./db";
 import { ZKPService } from "./service";
-import { concatThenHash } from "./util";
 
 export * as zkp_mgr from "./server";
 
 const main = async function() {
-  // const zok = new ZKPService();
-  // await zok.init();
-  // // await zok.compileCircuit("noopTest");
-  // const model = await zok.getCircuit("noopTest");
-  // console.log(schema.modelToProto(model).getArtifacts().getAbi());
-  console.log(concatThenHash([BigInt(0), BigInt(0), BigInt(0), BigInt(0)]));
+  const zok = new ZKPService();
+  await zok.init();
+  // await zok.compileCircuit("noopTest");
+  console.log((await zok.generateProof("noopTest", ["2"])));
 }
 
 main();
