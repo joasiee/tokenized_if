b5a69688e627d636e612326073e1d0541796f28b
Bjorn Ho <B.I.Y.L.Ho@student.tudelft.nl>
2021-04-08 23:59:22 +0200

    add comments services/index.ts

--- packages/tokenized_if/src/services/index.ts
+++ packages/tokenized_if/src/services/index.ts
@@ -7,8 +7,13 @@ import { addParticipant, getParticipant } from '../db/participant_queries';
 import { getTokenRegistry } from '../db/registry_queries';
 import { tm, tokenSetup } from './token';
 
+// Load in the .env file
 dotenv.config();
 
+/**
+ * A setup function which will switch cases depending on the role importer, financer or LSP.
+ * If it is the first run, then additional steps are needed.
+ */
 export async function setup() {
   const role = process.env.ROLE;
   const user = await getParticipant(process.env.NAME);
@@ -26,8 +31,10 @@ export async function setup() {
       await lsp.setup();
   }
 
+  // Retrieves token registry from the database, the tokenSetup function will branch, depending on if this token registry exists.
   await tokenSetup(await getTokenRegistry());
 
+  // Adds data into the database if it is the first run.
   if (firstRun) {
     const pubAddress = tm.signer.address;
     const natsEndpoint = `${process.env.NATS_URL}:${process.env.NATS_PORT}`;
@@ -38,4 +45,4 @@ export async function setup() {
       role: process.env.ROLE,
     });
   }
-};
+}
