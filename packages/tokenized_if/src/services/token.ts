2d5095886c040af6b95f8c26817c85189fe52889
Bjorn Ho <B.I.Y.L.Ho@student.tudelft.nl>
2021-04-08 23:54:02 +0200

    fix comments token.ts

--- packages/tokenized_if/src/services/token.ts
+++ packages/tokenized_if/src/services/token.ts
@@ -2,13 +2,22 @@ import { createTokenManager } from "../../../token_registry/"
 import dotenv from "dotenv";
 import { setTokenRegistry } from "../db/registry_queries";
 
+// Load in the .env file
 dotenv.config();
 
+/**
+ * tm is exported. So this TokenManager can be accessed from elsewhere.
+ * It contains the ganache URL and the private key, both loaded in from the .env file.
+ */
 export const tm = createTokenManager({
   ganacheUrl: process.env.GANACHE_URL,
   privateKey: process.env.PRIVATE_KEY,
 });
 
+/**
+ * The tokenSetup function to either deploy the token registry or to simply connect to it.
+ * @param registryAddress An optional token registry address.
+ */
 export async function tokenSetup(registryAddress?: string) {
   if (!registryAddress) {
     // ONLY EXECUTED BY LSP ON FIRST RUN
